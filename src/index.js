5cdc18397a2d4365710e90f007a4a83b75fc96d4
Brandon Zhu <bzhu@utraque.com>
2020-06-10 01:44:13 -0400

    173. Using Postman

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0-vxkoh.mongodb.net/<dbname>?retryWrites=true&w=majority';
 mongoose.connect(mongoUri, {
